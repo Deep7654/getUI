2e2796b7b233eba9de7c3f183cd68e50c2cd9004
Deepak Kumar <deep7654k@gmail.com>
2025-12-19 21:45:45 +0530

    Minor Change

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -2,13 +2,16 @@
 
 import { Project } from '@/db/models/project.model'
 import { Message } from '@/db/models/message.model'
+import dbConnect from '@/db/dbConnect/dbConnect';
 
 export async function getProject(id: string) {
+  dbConnect();
   const doc = await Project.findById(id)
   return JSON.parse(JSON.stringify(doc))
 }
 
 export async function getMessages(projectId: string) {
+  dbConnect();
   const docs = (await Message.find({ projectId }).populate("fragmentId"))
   return JSON.parse(JSON.stringify(docs))
 }
